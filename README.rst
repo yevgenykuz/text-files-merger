2afac2cd0c194bf31904d3d2ce0e9ad0b62975a8
Yevgeny Kuznetsov <Yevgeny.Kuznetsov@checkmarx.com>
2019-12-04 19:31:24 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,56 @@
+Text Files Merger
+#################
+
+This script does the following:
+
+1. Takes two or more input files
+2. [Optional] Alters prefix and suffix of each line from the input files (in memory, original files are untouched)
+3. Merges all lines, with or without duplicates
+4. Saves result to a new file
+
+-----
+
+
+.. contents::
+
+.. section-numbering::
+
+Usage
+=====
+This project requires:
+
+* Python >=3.6
+
+Clone the source code
+---------------------
+In your working folder (your home folder, for example)
+
+.. code-block:: bash
+
+    git clone https://github.com/yevgenykuz/text-files-merger.git
+
+Configure and run
+-----------------
+Run with (*see arguments*):
+
+.. code-block:: bash
+
+    python replace_strings_in_files.py
+
+
+Arguments
+---------
+.. code-block::
+
+
+Meta
+====
+Authors
+-------
+`yevgenykuz <https://github.com/yevgenykuz>`_
+
+License
+-------
+`MIT License <https://github.com/yevgenykuz/text-files-merger/blob/master/LICENSE>`_
+
+-----
---
+++ merge_files.py
@@ -0,0 +1,105 @@
+import argparse
+import glob
+import logging
+import os
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)-8s | %(message)s',
+                    datefmt='%d-%b-%Y %H:%M:%S', level=logging.INFO)
+log = logging.getLogger()
+
+args_parser = argparse.ArgumentParser(description='Merges two or more text files')
+args_parser.add_argument('-path', dest='path', default='files',
+                         help='The path to the directory that contains the files to merge (default is \'files\')')
+args_parser.add_argument('-ext', dest='file_extension', default='',
+                         help='The extension of the files to merge (merges everything by default)')
+args_parser.add_argument('-out', dest='out_file', default='output.txt',
+                         help='Merged lines output file name')
+args_parser.add_argument('-no_duplicates', dest='no_duplicates', action='store_true',
+                         help='Prevent duplicate lines')
+args_parser.add_argument('-sort', dest='sort', action='store_true',
+                         help='Sort merged lines')
+args_parser.add_argument('-prefix_before', dest='prefix_before', default='',
+                         help='The prefix to replace in each line before merging. -prefix_after must be provided')
+args_parser.add_argument('-prefix_after', dest='prefix_after', default='',
+                         help='The new prefix each line before merging. -prefix_before must be provided')
+args_parser.add_argument('-suffix_before', dest='suffix_before', default='',
+                         help='The suffix to replace in each line before merging. -suffix_after must be provided')
+args_parser.add_argument('-suffix_after', dest='suffix_after', default='',
+                         help='The new suffix each line before merging. -suffix_before must be provided')
+
+
+class TextFilesMerger:
+    """ Merges two or more text files, line by line.
+
+    Optional features:
+     - Remove duplicate lines
+     - Sort merged lines
+     - Prefix and suffix alteration before merging
+
+    Args:
+        path: The path to the directory that contains the files to merge
+        file_extension: The extension of the files to merge
+        out_file: Merged lines output file name
+        no_duplicates: If True, prevents duplicate lines
+        prefix_before: The prefix to replace in each line before merging. prefix_after must be provided
+        prefix_after: The new prefix each line before merging. prefix_before must be provided
+        suffix_before: The suffix to replace in each line before merging. suffix_after must be provided
+        suffix_after: The new suffix each line before merging. suffix_before must be provided
+    """
+
+    def __init__(self, path, file_extension, out_file, no_duplicates, sort, prefix_before, prefix_after, suffix_before,
+                 suffix_after):
+        self.path = path
+        self.file_extension = file_extension
+        self.out_file = out_file
+        self.no_duplicates = no_duplicates
+        self.sort = sort
+        self.prefix_before = prefix_before
+        self.prefix_after = prefix_after
+        self.suffix_before = suffix_before
+        self.suffix_after = suffix_after
+
+    def merge(self):
+        if not self.file_extension:
+            file_pattern = '**/*'
+            log.info('All files in \'{}\' will be merged.'.format(self.path))
+        else:
+            file_pattern = '**/*.{}'.format(self.file_extension)
+            log.info('\'{}\' files in \'{}\' will be merged.'.format(self.file_extension, self.path))
+
+        all_lines = []
+        for file in glob.glob(os.path.join(self.path, file_pattern), recursive=True):
+            if os.path.isdir(file):
+                continue
+            log.info('Merging lines from: {}'.format(file))
+            with open(file, 'r') as fi:
+                lines = fi.read().splitlines()
+                lines = [self._alter_prefix_and_suffix(line) for line in lines]
+                all_lines += lines
+
+        if self.no_duplicates:
+            log.info('Removing duplicate lines')
+            all_lines = set(all_lines)
+
+        if self.sort:
+            log.info('Sorting lines')
+            all_lines = sorted(all_lines)
+
+        with open(self.out_file, 'w') as fo:
+            fo.write("\n".join(all_lines))
+
+    def _alter_prefix_and_suffix(self, line):
+        if self.prefix_before and self.prefix_after:
+            line = line[len(self.prefix_before):] if line.startswith(self.prefix_before) else line
+        if self.suffix_before and self.suffix_after:
+            line = line[:-len(self.prefix_before)] if line.endswith(self.prefix_before) else line
+        return line
+
+
+if __name__ == '__main__':
+    args = args_parser.parse_args()
+    merger = TextFilesMerger(args.path, args.file_extension, args.out_file, args.no_duplicates, args.sort,
+                             args.prefix_before, args.prefix_after, args.suffix_before, args.suffix_after)
+    log.info('Merging files...')
+    merger.merge()
+    log.info('Done')
